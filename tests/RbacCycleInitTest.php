42eadc48881bd4bdbfed582bd26aba18775e1c47
Alexey Rogachev <arogachev90@gmail.com>
2023-03-03 14:09:47 +0600

    Check items table in details

--- tests/RbacCycleInitTest.php
+++ tests/RbacCycleInitTest.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Yiisoft\Rbac\Cycle\Tests;
 
+use Cycle\Database\Schema\AbstractIndex;
 use InvalidArgumentException;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Yiisoft\Rbac\Cycle\Command\RbacCycleInit;
+use Yiisoft\Rbac\Item;
 
 class RbacCycleInitTest extends TestCase
 {
@@ -59,7 +61,8 @@ class RbacCycleInitTest extends TestCase
         $output = new BufferedOutput(decorated: true);
         $app->find('rbac/cycle/init')->run(new ArrayInput([]), $output);
 
-        $this->assertTrue($this->getDbal()->database()->hasTable('auth_item'));
+        $this->checkItemsTable();
+
         $this->assertTrue($this->getDbal()->database()->hasTable('auth_item_child'));
         $this->assertTrue($this->getDbal()->database()->hasTable('auth_assignment'));
 
@@ -72,6 +75,56 @@ class RbacCycleInitTest extends TestCase
         "\033[42mTable `auth_assignment` created successfully\033[49m$newLine" .
         "\033[32mDONE\033[39m$newLine";
         $this->assertSame($expectedOutput, $output->fetch());
+    }
+
+    private function checkItemsTable(): void
+    {
+        $database = $this->getDbal()->database();
+        $this->assertTrue($database->hasTable('auth_item'));
+
+        $table = $database->table('auth_item');
+        $columns = $table->getColumns();
+
+        $this->assertArrayHasKey('name', $columns);
+        $name = $columns['name'];
+        $this->assertSame('string', $name->getType());
+        $this->assertSame(128, $name->getSize());
+        $this->assertTrue($name->isNullable());
+
+        $this->assertArrayHasKey('type', $columns);
+        $type = $columns['type'];
+        $this->assertSame('enum', $type->getAbstractType());
+        $this->assertSame([Item::TYPE_ROLE, Item::TYPE_PERMISSION], $type->getEnumValues());
+        $this->assertFalse($type->isNullable());
+
+        $this->assertArrayHasKey('description', $columns);
+        $description = $columns['description'];
+        $this->assertSame('string', $description->getType());
+        $this->assertSame(191, $description->getSize());
+        $this->assertTrue($description->isNullable());
+
+        $this->assertArrayHasKey('ruleName', $columns);
+        $ruleName = $columns['ruleName'];
+        $this->assertSame('string', $ruleName->getType());
+        $this->assertSame(64, $ruleName->getSize());
+        $this->assertTrue($ruleName->isNullable());
+
+        $this->assertArrayHasKey('createdAt', $columns);
+        $createdAt = $columns['createdAt'];
+        $this->assertSame('timestamp', $createdAt->getAbstractType());
+        $this->assertFalse($createdAt->isNullable());
+
+        $this->assertArrayHasKey('updatedAt', $columns);
+        $updatedAt = $columns['updatedAt'];
+        $this->assertSame('timestamp', $updatedAt->getAbstractType());
+        $this->assertFalse($updatedAt->isNullable());
+
+        $this->assertCount(1, $table->getIndexes());
+        /** @var AbstractIndex $index */
+        $index = array_values($table->getIndexes())[0];
+        $this->assertSame(['type'], $index->getColumns());
+
+        $this->assertSame(['name'], $table->getPrimaryKeys());
     }
 
     protected function populateDb(): void
