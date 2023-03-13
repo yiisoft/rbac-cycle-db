9fbbdc8e69eca059f422be49f320e36d0609843c
Sobhan <44964722+sobhan-m94@users.noreply.github.com>
2023-03-13 17:13:16 +0330

    Update RbacCycleInit.php

--- src/Command/RbacCycleInit.php
+++ src/Command/RbacCycleInit.php
@@ -83,7 +83,7 @@ final class RbacCycleInit extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $force = $input->getOption('force') ?? true;
+        $force = (bool)($input->getOption('force') ?? true);
         if ($force) {
             $this->dropTable($this->itemsChildrenTable, $output);
             $this->dropTable($this->assignmentsTable, $output);
