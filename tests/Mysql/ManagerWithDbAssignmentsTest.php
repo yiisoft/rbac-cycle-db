ebd9081c07b3a69492f0142476502c3f0d31c76c
Alexey Rogachev <arogachev90@gmail.com>
2023-07-24 17:46:44 +0600

    Add manager tests for other RDBMS

---
+++ tests/Mysql/ManagerWithDbAssignmentsTest.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Rbac\Cycle\Tests\Mysql;
+
+class ManagerWithDbAssignmentsTest extends \Yiisoft\Rbac\Cycle\Tests\Base\ManagerWithDbAssignmentsTest
+{
+    use DatabaseTrait;
+}
