964dc03921fa90c1f77e74fb1c6b41240f3170b5
Alexey Rogachev <arogachev90@gmail.com>
2023-07-24 17:35:11 +0600

    Add coupled manager test [skip ci]

---
+++ tests/Base/ManagerWithDbItemsAndAssignmentsTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Rbac\Cycle\Tests\Base;
+
+use Yiisoft\Rbac\AssignmentsStorageInterface;
+use Yiisoft\Rbac\Cycle\AssignmentsStorage;
+use Yiisoft\Rbac\Cycle\ItemsStorage;
+use Yiisoft\Rbac\Cycle\Manager;
+use Yiisoft\Rbac\ItemsStorageInterface;
+use Yiisoft\Rbac\RuleFactoryInterface;
+use Yiisoft\Rbac\Tests\Common\ManagerTestTrait;
+use Yiisoft\Rbac\Tests\Support\SimpleRuleFactory;
+
+abstract class ManagerWithDbItemsAndAssignmentsTest extends TestCase
+{
+    use ManagerTestTrait {
+        setUp as protected traitSetUp;
+        tearDown as protected traitTearDown;
+    }
+
+    protected function setUp(): void
+    {
+        $this->createSchemaManager()->ensureTables();
+        $this->traitSetUp();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->createSchemaManager()->ensureNoTables();
+        $this->traitTearDown();
+    }
+
+    protected function populateDatabase(): void
+    {
+        // Skip
+    }
+
+    protected function createManager(
+        ?ItemsStorageInterface $itemsStorage = null,
+        ?AssignmentsStorageInterface $assignmentsStorage = null,
+        ?RuleFactoryInterface $ruleFactory = null,
+        bool $enableDirectPermissions = false
+    ): Manager {
+        return new Manager(
+            $itemsStorage ?? $this->createItemsStorage(),
+            $assignmentsStorage ?? $this->createAssignmentsStorage(),
+            $ruleFactory ?? new SimpleRuleFactory(),
+            $this->getDatabase(),
+            $enableDirectPermissions,
+        );
+    }
+
+    protected function createItemsStorage(): ItemsStorageInterface
+    {
+        return new ItemsStorage(self::ITEMS_TABLE, $this->getDatabase(), self::ITEMS_CHILDREN_TABLE);
+    }
+
+    protected function createAssignmentsStorage(): AssignmentsStorageInterface
+    {
+        return new AssignmentsStorage(self::ASSIGNMENTS_TABLE, $this->getDatabase());
+    }
+}
