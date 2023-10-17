447feca76cf8b3c6cd21a872f0322bf65ea0f605
Alexey Rogachev <arogachev90@gmail.com>
2023-10-17 12:41:51 +0600

    Apply Tigrov's latest suggestion

--- src/ItemTreeTraversal/MysqlItemTreeTraversal.php
+++ src/ItemTreeTraversal/MysqlItemTreeTraversal.php
@@ -103,7 +103,7 @@ final class MysqlItemTreeTraversal implements ItemTreeTraversalInterface
     {
         $fromSql = "SELECT DISTINCT child
         FROM (SELECT * FROM $this->childrenTableName ORDER by parent) item_child_sorted,
-        (SELECT @pv := :name) init
+        (SELECT @pv := ?) init
         WHERE find_in_set(parent, @pv) AND length(@pv := concat(@pv, ',', child))";
         $outerQuery = $baseOuterQuery
             ->from(new Fragment("($fromSql) s"))
@@ -112,8 +112,6 @@ final class MysqlItemTreeTraversal implements ItemTreeTraversalInterface
         /** @psalm-var non-empty-string $outerQuerySql */
         $outerQuerySql = (string) $outerQuery;
 
-        var_dump((string) $this->database->query($outerQuerySql, [':name' => $name]));
-
-        return $this->database->query($outerQuerySql, [':name' => $name]);
+        return $this->database->query($outerQuerySql, [$name]);
     }
 }
