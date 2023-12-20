c491aa2be2fb5c0773c2ed5f0c545aa939908707
Alexey Rogachev <arogachev90@gmail.com>
2023-12-20 18:02:21 +0600

    Fix scrutinizer and MySQL 5

--- src/ItemTreeTraversal/MysqlItemTreeTraversal.php
+++ src/ItemTreeTraversal/MysqlItemTreeTraversal.php
@@ -41,9 +41,8 @@ final class MysqlItemTreeTraversal implements ItemTreeTraversalInterface
     {
         $sql = "SELECT DISTINCT item.* FROM (
             SELECT @r AS child_name,
-            (SELECT @r := parent FROM $this->childrenTableName WHERE child = child_name LIMIT 1) AS parent,
-            @l := @l + 1 AS level
-            FROM (SELECT @r := :name, @l := 0) val, $this->childrenTableName
+            (SELECT @r := parent FROM $this->childrenTableName WHERE child = child_name LIMIT 1) AS parent
+            FROM (SELECT @r := :name) val, $this->childrenTableName
         ) s
         LEFT JOIN $this->tableName AS item ON item.name = s.child_name
         WHERE item.name != :name";
@@ -55,21 +54,33 @@ final class MysqlItemTreeTraversal implements ItemTreeTraversalInterface
             ->fetchAll();
     }
 
+    public function getAccessTree(string $name): array
+    {
+        $sql = "SELECT item.*, access_tree_base.children FROM (
+            SELECT child_name, MIN(TRIM(BOTH ',' FROM TRIM(BOTH child_name FROM raw_children))) as children FROM (
+                SELECT @r AS child_name, @path := concat(@path, ',', @r) as raw_children,
+                (SELECT @r := parent FROM $this->childrenTableName WHERE child = child_name LIMIT 1) AS parent
+                FROM (SELECT @r := :name, @path := '') val, $this->childrenTableName
+            ) raw_access_tree_base
+            GROUP BY child_name
+        ) access_tree_base
+        LEFT JOIN $this->tableName AS item ON item.name = access_tree_base.child_name";
+
+        return $this
+            ->database
+            ->query($sql, [':name' => $name])
+            ->fetchAll();
+    }
+
     public function getChildrenRows(string|array $names): array
     {
-        $baseOuterQuery = $this->database->select([new Fragment('item.*')])->distinct();
-
         /** @psalm-var RawItem[] */
-        return $this->getChildrenRowsStatement($names, baseOuterQuery: $baseOuterQuery)->fetchAll();
+        return $this->getChildrenRowsStatement($names, baseOuterQuery: $this->getChildrenBaseOuterQuery())->fetchAll();
     }
 
     public function getChildPermissionRows(string|array $names): array
     {
-        $baseOuterQuery = $this
-            ->database
-            ->select(['item.*'])
-            ->distinct()
-            ->where(['item.type' => Item::TYPE_PERMISSION]);
+        $baseOuterQuery = $this->getChildrenBaseOuterQuery()->where(['item.type' => Item::TYPE_PERMISSION]);
 
         /** @psalm-var RawItem[] */
         return $this->getChildrenRowsStatement($names, baseOuterQuery: $baseOuterQuery)->fetchAll();
@@ -77,11 +88,7 @@ final class MysqlItemTreeTraversal implements ItemTreeTraversalInterface
 
     public function getChildRoleRows(string|array $names): array
     {
-        $baseOuterQuery = $this
-            ->database
-            ->select(['item.*'])
-            ->distinct()
-            ->where(['item.type' => Item::TYPE_ROLE]);
+        $baseOuterQuery = $this->getChildrenBaseOuterQuery()->where(['item.type' => Item::TYPE_ROLE]);
 
         /** @psalm-var RawItem[] */
         return $this->getChildrenRowsStatement($names, baseOuterQuery: $baseOuterQuery)->fetchAll();
@@ -101,17 +108,43 @@ final class MysqlItemTreeTraversal implements ItemTreeTraversalInterface
 
     private function getChildrenRowsStatement(string|array $names, SelectQuery $baseOuterQuery): StatementInterface
     {
+        $names = (array) $names;
         $fromSql = "SELECT DISTINCT child
-        FROM (SELECT * FROM $this->childrenTableName ORDER by parent) item_child_sorted,
-        (SELECT @pv := ?) init
-        WHERE find_in_set(parent, @pv) AND length(@pv := concat(@pv, ',', child))";
+        FROM (SELECT * FROM $this->childrenTableName ORDER by parent) item_child_sorted,\n";
+        $where = '';
+        $parameters = [];
+        $lastNameIndex = array_key_last($names);
+
+        foreach ($names as $index => $name) {
+            $fromSql .= "(SELECT @pv$index := :name$index) init$index";
+            if ($index !== $lastNameIndex) {
+                $fromSql .= ',';
+            }
+
+            $fromSql .= "\n";
+
+            if ($index !== 0) {
+                $where .= ' OR ';
+            }
+
+            $where .= "(find_in_set(parent, @pv$index) AND length(@pv$index := concat(@pv$index, ',', child)))";
+
+            $parameters[":name$index"] = $name;
+        }
+
+        $fromSql .= "WHERE $where";
         $outerQuery = $baseOuterQuery
-            ->from(new Fragment("($fromSql) s", [$names]))
+            ->from(new Fragment("($fromSql) s"))
             ->leftJoin($this->tableName, 'item')
             ->on('item.name', 's.child');
         /** @psalm-var non-empty-string $outerQuerySql */
         $outerQuerySql = (string) $outerQuery;
 
-        return $this->database->query($outerQuerySql, [':name' => $names]);
+        return $this->database->query($outerQuerySql, $parameters);
+    }
+
+    private function getChildrenBaseOuterQuery(): SelectQuery
+    {
+        return  $this->database->select('item.*')->distinct();
     }
 }
